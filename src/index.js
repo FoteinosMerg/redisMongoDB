27a077d4739558df2f2e90f53226f4cefba4e08a
FoteinosMerg <foteinosmerg@gmail.com>
2018-10-02 08:55:28 +0200

    MongoDB connection restructured

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 
-mongoose.connect(
-  keys.mongoURI,
-  { useNewUrlParser: true }
-);
+mongoose
+  .connect(
+    keys.mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log(" * MongoDB successfully connected"))
+  .catch(err => console.log(" * MongoDB connection refused"));
 
 const app = express();
 require("./routes/routesFile")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log("\n * Minimal Express setup with MongoDB supported by Redis")
+  console.log("\n * Minimal express setup with MongoDB supported by Redis")
 );
